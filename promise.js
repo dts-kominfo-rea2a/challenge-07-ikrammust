f2a526be17ad4619c93607741b875d87ac46772f
Ikram Mustofa Kamal <ikram.mustofa07@gmail.com>
2022-06-12 23:45:25 +0700

    refactor: fix a small mistake in async function promiseOutput()

--- promise.js
+++ promise.js
@@ -1,36 +1,31 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = (emosi) => {
-  let emosiPenonton = async() => {
-    try {
-      // read data from promiseTheaterIXX
-      let dataFilmIXX = await promiseTheaterIXX();
-      // read data from promiseTheaterVGC
-      let dataFilmVGC = await promiseTheaterVGC();
+const promiseOutput = async (emosi) => {
+  try {
+    // read data from promiseTheaterIXX
+    let dataFilmIXX = await promiseTheaterIXX();
+    // read data from promiseTheaterVGC
+    let dataFilmVGC = await promiseTheaterVGC();
 
-      // make it one array
-      let allDataFilm = [...dataFilmIXX, ...dataFilmVGC];
-      // counter for audience emotion
-      let emosiCounter = 0;
+    // make it one array
+    let allDataFilm = [...dataFilmIXX, ...dataFilmVGC];
+    // counter for audience emotion
+    let emosiCounter = 0;
 
-      // calculate audience emotion
-      allDataFilm.forEach((element) => {
-        if(element.hasil === emosi) {
-          emosiCounter++
-        }
-      });
+    // calculate audience emotion
+    allDataFilm.forEach((element) => {
+      if(element.hasil === emosi) {
+        emosiCounter++
+      }
+    });
 
-      // return how much film that make audience angry or not
-      return emosiCounter;
-    } catch(err) {
-      // if some error happens, throw that error.
-      console.log(err);
-    }
-  };
-
-  // call the audience emotion counter
-  return emosiPenonton();
+    // return how much film that make audience angry or not
+    return emosiCounter;
+  } catch(err) {
+    // if some error happens, throw that error.
+    console.log(err);
+  }
 };
 
 module.exports = {
